1c653d3770769944323a049ddb2af9ee0c636b37
andresmauricio <andresacelas@outlook.com>
2020-06-03 23:31:58 -0500

    form answers

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatCardModule} from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 
 
+const modules = [
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule,
+  MatInputModule,
+  MatButtonModule,
+  MatFormFieldModule
+];
 
 @NgModule({
   declarations: [],
-  imports: [MatToolbarModule, MatIconModule, MatCardModule],
-  exports: [MatToolbarModule, MatIconModule, MatCardModule]
+  imports: modules,
+  exports: modules,
 })
-export class MaterialModule { }
+export class MaterialModule {}
--- src/app/questions/questions.module.ts
+++ src/app/questions/questions.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import { QuestionDetailComponent } from './question-detail/question-detail.component';
 import { MaterialModule } from '../material.module';
 import { MomentModule } from 'angular2-moment';
+import { AnswerFormComponent } from './answer-form/answer-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [QuestionDetailComponent],
+  declarations: [QuestionDetailComponent, AnswerFormComponent],
   imports: [
     CommonModule,
+    FormsModule,
     MaterialModule,
     MomentModule
   ],
