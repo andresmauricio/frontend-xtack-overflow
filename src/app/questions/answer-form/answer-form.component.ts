df8df8ad7fb8c1fd34d3ce425a16a3a1d7fee4f4
andresmauricio <andresacelas@outlook.com>
2020-06-04 21:40:52 -0500

    add answer model create

--- src/app/questions/answer-form/answer-form.component.ts
+++ src/app/questions/answer-form/answer-form.component.ts
@@ -1,17 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Answer, User } from './answer.model';
+import { Question } from '../question.model';
 
 @Component({
   selector: 'app-answer-form',
   templateUrl: './answer-form.component.html',
-  styleUrls: ['./answer-form.component.css']
+  styleUrls: ['./answer-form.component.css']  
 })
 export class AnswerFormComponent implements OnInit {
 
   constructor() { }
+  @Input() question: Question;
 
   onSumbmit(form: NgForm) {
-    console.log(form.value.description);
+    const answer = new Answer(
+      form.value.description,
+      this.question,
+      new Date(),
+      new User('Palua', 'Vargas')
+    )
+    this.question.answers.unshift(answer);
+    form.reset();
     
   }
 
--- src/app/questions/question.model.ts
+++ src/app/questions/question.model.ts
@@ -1,13 +1,17 @@
+import { Answer } from './answer-form/answer.model';
+
 export class Question {
     public title: string;
     public description: string;
     public createdAt?: Date | string;
     public icon?: string;
+    public answers: Answer[];
 
     constructor(title: string, description: string, createdAt?: Date | string, icon?: string) {
         this.title = title;
         this.description= description;
         this.createdAt = createdAt;
         this.icon = icon;
+        this.answers = []
     }
 }
