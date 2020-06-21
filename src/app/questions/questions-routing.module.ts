11d3814337ad4b14e341a39847e85688157b0560
andresmauricio <andresacelas@outlook.com>
2020-06-21 11:27:50 -0500

    Agregar nueva pregunta

--- src/app/questions/questions-routing.module.ts
+++ src/app/questions/questions-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { QuestionListComponent } from './question-list/question-list.component';
 import { QuestionDetailComponent } from './question-detail/question-detail.component';
+import { QuestionFormComponent } from './question-form/question-form.component';
 
 
 const routes: Routes = [
   { path: '', component: QuestionListComponent },
   { path: ':id', component: QuestionDetailComponent },
+  { path: 'new', component: QuestionFormComponent },
 //   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
